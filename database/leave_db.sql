d6365c5cf2f71bb860b13e9ca12c77e4005a9991
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2023-04-17 09:32:15 +0800

    updated database commit

--- database/leave_db.sql
+++ database/leave_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2021 at 11:21 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Apr 17, 2023 at 03:31 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,7 @@ CREATE TABLE `department_list` (
   `description` text NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `department_list`
@@ -42,7 +42,8 @@ CREATE TABLE `department_list` (
 INSERT INTO `department_list` (`id`, `name`, `description`, `date_created`, `date_updated`) VALUES
 (1, 'HR Department', 'Human Resource Department', '2021-08-21 10:13:45', NULL),
 (2, 'IT Department', 'Information Technology Department', '2021-08-21 10:16:06', NULL),
-(3, 'Accounting & Finance Dept.', 'Accounting and Finance Department', '2021-08-21 10:18:00', '2021-08-21 10:18:17');
+(3, 'Accounting & Finance Dept.', 'Accounting and Finance Department', '2021-08-21 10:18:00', '2021-08-21 10:18:17'),
+(4, 'Asset Management Unit', 'Asset Management Unit Supply', '2023-04-11 11:31:10', NULL);
 
 -- --------------------------------------------------------
 
@@ -56,7 +57,7 @@ CREATE TABLE `designation_list` (
   `description` text NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `designation_list`
@@ -66,7 +67,8 @@ INSERT INTO `designation_list` (`id`, `name`, `description`, `date_created`, `da
 (1, 'Staff', 'Office Clerk', '2021-08-21 10:22:03', NULL),
 (2, 'Programmer', 'Programmer', '2021-08-21 10:22:35', NULL),
 (3, 'Web Developer', 'Web Developer', '2021-08-21 10:22:52', NULL),
-(4, 'HR Staff', 'Human Resource Staff', '2021-08-21 10:23:18', NULL);
+(4, 'HR Staff', 'Human Resource Staff', '2021-08-21 10:23:18', NULL),
+(5, 'Administrative Officer IV', 'Supply Officer in charge', '2023-04-11 11:32:18', NULL);
 
 -- --------------------------------------------------------
 
@@ -79,7 +81,7 @@ CREATE TABLE `employee_meta` (
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employee_meta`
@@ -98,8 +100,36 @@ INSERT INTO `employee_meta` (`user_id`, `meta_field`, `meta_value`, `date_create
 (11, 'designation_id', '3', '2021-08-21 14:48:00'),
 (11, 'username', 'jsmith', '2021-08-21 14:48:00'),
 (11, 'approver', 'on', '2021-08-21 14:48:00'),
-(11, 'leave_type_ids', '3,2,1', '2021-08-21 14:48:06'),
-(11, 'leave_type_credits', '{\"3\":\"365\",\"2\":\"5\",\"1\":\"10\"}', '2021-08-21 14:48:06');
+(16, 'type', '3', '2023-04-11 15:10:26'),
+(16, 'employee_id', '0000155', '2023-04-11 15:10:26'),
+(16, 'firstname', 'NICOLE BRANDON', '2023-04-11 15:10:26'),
+(16, 'middlename', 'VIRAY', '2023-04-11 15:10:26'),
+(16, 'lastname', 'DECENA', '2023-04-11 15:10:26'),
+(16, 'dob', '1996-09-03', '2023-04-11 15:10:26'),
+(16, 'contact', '09685013431', '2023-04-11 15:10:26'),
+(16, 'address', '378 Pangapisan North Lingayen, Pangasinan', '2023-04-11 15:10:26'),
+(16, 'department_id', '2', '2023-04-11 15:10:26'),
+(16, 'designation_id', '2', '2023-04-11 15:10:26'),
+(16, 'username', 'nicole.decena', '2023-04-11 15:10:26'),
+(16, 'approver', 'off', '2023-04-11 15:10:26'),
+(11, 'leave_type_ids', '4,3,2,1', '2023-04-11 17:15:59'),
+(11, 'leave_type_credits', '{\"4\":\"110\",\"3\":\"365\",\"2\":\"5\",\"1\":\"10\"}', '2023-04-11 17:15:59'),
+(17, 'type', '3', '2023-04-12 10:56:35'),
+(17, 'employee_id', '100023', '2023-04-12 10:56:35'),
+(17, 'firstname', 'Kenneth Rey', '2023-04-12 10:56:35'),
+(17, 'middlename', 'Rallustian', '2023-04-12 10:56:35'),
+(17, 'lastname', 'Tablang', '2023-04-12 10:56:35'),
+(17, 'dob', '2001-06-12', '2023-04-12 10:56:35'),
+(17, 'contact', '09958123933', '2023-04-12 10:56:35'),
+(17, 'address', 'Brgy. Bolaney alaminos city pangasinan', '2023-04-12 10:56:35'),
+(17, 'department_id', '2', '2023-04-12 10:56:35'),
+(17, 'designation_id', '2', '2023-04-12 10:56:35'),
+(17, 'username', '100023', '2023-04-12 10:56:35'),
+(17, 'approver', 'off', '2023-04-12 10:56:35'),
+(17, 'leave_type_ids', '12,16,6,8,17,13,15,7,14,10,9,11,18,5', '2023-04-12 10:56:41'),
+(17, 'leave_type_credits', '{\"12\":\"20\",\"16\":\"20\",\"6\":\"20\",\"8\":\"120\",\"17\":\"30\",\"13\":\"50\",\"15\":\"30\",\"7\":\"20\",\"14\":\"20\",\"10\":\"20\",\"9\":\"20\",\"11\":\"300\",\"18\":\"30\",\"5\":\"20\"}', '2023-04-12 10:56:41'),
+(16, 'leave_type_ids', '12,16,6,8,17,13,15,7,14,10,9,11,18,5', '2023-04-12 12:51:55'),
+(16, 'leave_type_credits', '{\"12\":\"20\",\"16\":\"20\",\"6\":\"20\",\"8\":\"120\",\"17\":\"30\",\"13\":\"50\",\"15\":\"30\",\"7\":\"20\",\"14\":\"20\",\"10\":\"20\",\"9\":\"20\",\"11\":\"300\",\"18\":\"30\",\"5\":\"20\"}', '2023-04-12 12:51:55');
 
 -- --------------------------------------------------------
 
@@ -120,17 +150,18 @@ CREATE TABLE `leave_applications` (
   `leave_days` float NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `leave_applications`
 --
 
 INSERT INTO `leave_applications` (`id`, `user_id`, `leave_type_id`, `reason`, `date_start`, `date_end`, `type`, `status`, `approved_by`, `leave_days`, `date_created`, `date_updated`) VALUES
-(1, 11, 2, 'Sample only', '2021-08-23 00:00:00', '2021-08-25 00:00:00', 1, 2, 1, 3, '2021-08-21 16:17:04', '2021-08-21 16:36:12'),
-(2, 11, 2, 'Test', '2021-08-21 00:00:00', '2021-08-21 00:00:00', 2, 1, 1, 0.5, '2021-08-21 16:22:08', '2021-08-21 16:35:56'),
-(3, 11, 2, 'Test Re apply', '2021-08-23 00:00:00', '2021-08-23 00:00:00', 1, 1, 1, 1, '2021-08-21 16:38:41', '2021-08-21 16:38:49'),
-(4, 11, 1, 'Vacation', '2021-08-31 00:00:00', '2021-09-02 00:00:00', 1, 0, NULL, 3, '2021-08-21 17:20:07', NULL);
+(1, 11, NULL, 'Sample only', '2021-08-23 00:00:00', '2021-08-25 00:00:00', 1, 2, 1, 3, '2021-08-21 16:17:04', '2021-08-21 16:36:12'),
+(2, 11, NULL, 'Test', '2021-08-21 00:00:00', '2021-08-21 00:00:00', 2, 1, 1, 0.5, '2021-08-21 16:22:08', '2021-08-21 16:35:56'),
+(3, 11, NULL, 'Test Re apply', '2021-08-23 00:00:00', '2021-08-23 00:00:00', 1, 1, 1, 1, '2021-08-21 16:38:41', '2021-08-21 16:38:49'),
+(4, 11, NULL, 'Vacation', '2021-08-31 00:00:00', '2021-09-02 00:00:00', 1, 0, NULL, 3, '2021-08-21 17:20:07', NULL),
+(12, 17, 16, 'no reason', '2023-04-13 00:00:00', '2023-04-14 00:00:00', 1, 1, 1, 2, '2023-04-12 11:04:56', '2023-04-12 11:06:00');
 
 -- --------------------------------------------------------
 
@@ -147,17 +178,27 @@ CREATE TABLE `leave_types` (
   `status` tinyint(1) NOT NULL DEFAULT 1,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `leave_types`
 --
 
 INSERT INTO `leave_types` (`id`, `code`, `name`, `description`, `default_credit`, `status`, `date_created`, `date_updated`) VALUES
-(1, 'VL', 'Vacation Leave', 'Vacation Leave with Pay', 10, 1, '2021-08-21 10:39:47', NULL),
-(2, 'SL', 'Sick Leave', 'Leave type for calling sick with pay', 5, 1, '2021-08-21 13:48:03', NULL),
-(3, 'LWOP', 'Leave w/o Pay', 'Leave w/o Pay', 999, 1, '2021-08-21 13:49:08', NULL);
-
+(5, 'VL', 'Vacation Leave', 'Sec. 51, Rule XVI, Omnibus Rules Implementing E.O. No. 292', 20, 1, '2023-04-12 08:33:17', NULL),
+(6, 'M/FL', 'Mandatory/Forced Leave', 'Sec. 25, Rule XVI, Omnibus Rules Implementing E.O. 292', 20, 1, '2023-04-12 08:36:45', NULL),
+(7, 'SL', 'Sick Leave', 'Sec. 43, Rule XVI, Omnibus Rules Implementing E.O. No. 292', 20, 1, '2023-04-12 08:37:57', NULL),
+(8, 'ML', 'Maternity Leave', 'R.A. No. 11210 / IRR issued by CSC, DOLE and SSS', 120, 1, '2023-04-12 09:05:27', NULL),
+(9, 'Spec. Priv. ', 'Special Privilege Leave', 'Sec. 21, Rule XVI, Omnibus Rules Implementing E.O. No. 292', 20, 1, '2023-04-12 09:07:29', NULL),
+(10, 'Solo Par.', 'Solo Parent Leave ', 'R.A. No 8972 / CSC MC No. 8, s. 2004', 20, 1, '2023-04-12 09:13:06', NULL),
+(11, 'Stud. L.', 'Study Leave', 'Sec. 68, Rule XVI, Omnibus Rules Implementing E.O. No. 292', 300, 1, '2023-04-12 09:14:35', NULL),
+(12, '10-D VAWC L', '10-Day VAWC Leave', 'R,A. No. 9262 / CSC MC No. 15, s 2005', 20, 1, '2023-04-12 09:15:57', NULL),
+(13, 'Rehab. Priv.', 'Rehabilitation Privilege', 'Sec. 55, Rule XVI, Omnibus Rules Implementing E.O. No. 292', 50, 1, '2023-04-12 09:17:09', NULL),
+(14, 'SLBW', 'Special Leave Benefit for Women', 'R.A. No. 9710 / CSC MC No. 25, s. 2010 ', 20, 1, '2023-04-12 09:19:08', NULL),
+(15, 'SECL', 'Special Emergency/Calamity Leave', 'CSC MC No. 2, 2012, as amended', 30, 1, '2023-04-12 09:20:10', '2023-04-12 11:04:09'),
+(16, 'Adoption Leave', 'Adoption Leave', 'R.A. No. 8552', 20, 1, '2023-04-12 09:20:51', '2023-04-12 11:03:26'),
+(17, 'Monetization of Leave Credits', 'Monetization of Leave Credits', 'Monetization of Leave Credits', 30, 1, '2023-04-12 09:22:33', '2023-04-12 10:30:58'),
+(18, 'Terminal Leave', 'Terminal Leave', 'Terminal Leave', 30, 1, '2023-04-12 09:23:00', '2023-04-12 10:30:10');
 
 -- --------------------------------------------------------
 
@@ -169,18 +210,18 @@ CREATE TABLE `system_info` (
   `id` int(30) NOT NULL,
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `system_info`
 --
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
-(1, 'name', 'Online Leave Management System - PHP'),
-(6, 'short_name', 'OLMS - PHP'),
-(11, 'logo', 'uploads/1629510720_company_logo.png'),
+(1, 'name', 'DEPED SDO1 Leave Management System'),
+(6, 'short_name', 'SDO1 Pangasinan - LMS'),
+(11, 'logo', 'uploads/1681178700_sdo1 logo.png'),
 (13, 'user_avatar', 'uploads/user_avatar.jpg'),
-(14, 'cover', 'uploads/1629510720_company.jpg');
+(14, 'cover', 'uploads/1681178700_sdo1building.jpg');
 
 -- --------------------------------------------------------
 
@@ -200,15 +241,17 @@ CREATE TABLE `users` (
   `type` tinyint(1) NOT NULL DEFAULT 0,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1624240500_avatar.png', NULL, 1, '2021-01-20 14:02:37', '2021-08-21 12:48:19'),
-(11, 'John', 'D', 'Smith', 'jsmith', '04e4ff14103f267c4b443362cefe9570', 'uploads/11_user.png', NULL, 3, '2021-08-21 12:08:07', '2021-08-21 12:22:45');
+(1, 'Administrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1681196880_243444773_228417245998590_5133413802818623704_n-removebg-preview.png', NULL, 1, '2021-01-20 14:02:37', '2023-04-11 15:08:26'),
+(11, 'John', 'D', 'Smith', 'jsmith', '04e4ff14103f267c4b443362cefe9570', 'uploads/11_user.png', NULL, 3, '2021-08-21 12:08:07', '2021-08-21 12:22:45'),
+(16, 'NICOLE BRANDON', 'VIRAY', 'DECENA', 'nicole.decena', '42603334c3a9e504e99a2330c6783ca0', 'uploads/16_user.jpg', NULL, 3, '2023-04-11 15:10:26', '2023-04-11 15:13:03'),
+(17, 'Kenneth Rey', 'Rallustian', 'Tablang', '100023', '65feb6b8c9726133b18ac2f2ac26e8bc', 'uploads/17_user.jpg', NULL, 3, '2023-04-12 10:56:35', '2023-04-12 10:56:35');
 
 --
 -- Indexes for dumped tables
@@ -246,7 +289,6 @@ ALTER TABLE `leave_applications`
 ALTER TABLE `leave_types`
   ADD PRIMARY KEY (`id`);
 
-
 --
 -- Indexes for table `system_info`
 --
@@ -267,25 +309,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `department_list`
 --
 ALTER TABLE `department_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `designation_list`
 --
 ALTER TABLE `designation_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `leave_applications`
 --
 ALTER TABLE `leave_applications`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `leave_types`
 --
 ALTER TABLE `leave_types`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `system_info`
@@ -297,7 +339,7 @@ ALTER TABLE `system_info`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
